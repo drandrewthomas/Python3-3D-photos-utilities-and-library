c936475e8fac02a448ed3366c5c811829f87019c
Andrew Thomas <drandrewthomas@users.noreply.github.com>
2025-03-21 15:30:59 +0000

    Add files via upload

--- batchprocess.py
+++ batchprocess.py
@@ -3,7 +3,7 @@ batchprocess.py - An example utility script for batch processing a number of 3D 
 
 REMEMBER THAT PHOTOS IN THE IMAGESTOPROCESS FOLDER WILL BE OVERWRITTEN BY SOME OPERATIONS SO BE VERY CAREFUL!
 
-Currently using MPO, JPG, JPEG or JPS extensions only.
+Currently using PNG, MPO, JPG, JPEG or JPS extensions only.
 """
 
 import glob
@@ -13,13 +13,13 @@ from photos3d import sbs as sbsim
 from photos3d import anaglyph as glyph
 from photos3d import image as img
 
-def get_file_list(exts=["jpg", " jpeg", "jps"]):
+def get_file_list(exts=["jpg", " jpeg", "jps", "png"]):
     exl = []
     for ext in exts:
         if ext.lower() not in exl:
-            exl.append(ext.lower())
+            exl.append(ext.lower().strip())
         if ext.upper() not in exl:
-            exl.append(ext.upper())
+            exl.append(ext.upper().strip())
     tflist = [] # Handle duplicates on Windows
     for ex in exl:
         dirpath = os.path.join(".", "imagestoprocess", "*." + ex)
@@ -40,8 +40,9 @@ def do_menu():
         print("b. Convert side-by-side JPEG files to anaglyphs.")
         print("c. Convert side-by-side JPEG files to mono anaglyphs.")
         print("d. Swap side-by-side left and right views.")
-        print("e. Pad to square (1:1) aspect ratio.")
-        print("f. Pad to double square (2:1) aspect ratio.")
+        print("e. Add mirrored bottoms to side-by-side images.")
+        print("f. Pad to square (1:1) aspect ratio.")
+        print("g. Pad to double square (2:1) aspect ratio.")
         print("x. Exit.")
         print()
         ch = input('Enter a choice: ').lower()
@@ -55,8 +56,10 @@ def do_menu():
         elif ch == 'd':
             batch_swap_sides()
         elif ch == 'e':
-            batch_aspect_ratio(1)
+            batch_mirror_bottom()
         elif ch == 'f':
+            batch_aspect_ratio(1)
+        elif ch == 'g':
             batch_aspect_ratio(2)
         print()
         print()
@@ -91,7 +94,7 @@ def batch_aspect_ratio(aspect, quality=95):
     for fname in images:
         fn, fext = os.path.splitext(fname)
         fext = fext.lower()
-        if fext == ".jpg" or fext == ".jps":
+        if fext == ".jpg" or fext == ".jps" or fext == ".png":
             print("Converting file: " + fname)
             oimg = img.open(fname)
             aimg = img.change_aspect_ratio(aspect, oimg)
@@ -105,6 +108,18 @@ def batch_swap_sides(quality=95):
         print("Swapping file: " + fname)
         lim, rim = sbsim.load(fname, dosplit=True)
         sbs = sbsim.create(rim, lim)
+        sbs.save(fname, quality=quality)
+
+def batch_mirror_bottom(quality=95):
+    images = get_file_list()
+    print("Adding mirrored bottom for " + str(len(images)) + " files:")
+    print()
+    for fname in images:
+        print("Bottom mirroring file: " + fname)
+        lim, rim = sbsim.load(fname, dosplit=True)
+        sbs = sbsim.create(lim, rim)
+        bsbs = sbsim.create(rim, lim)
+        sbs = sbsim.create(sbs, bsbs, mode='ud')
         sbs.save(fname, quality=quality)
 
 
