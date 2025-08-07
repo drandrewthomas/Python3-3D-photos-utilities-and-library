c209d8a1b29e11fec40425639607ae8c07cb695f
Andrew Thomas <drandrewthomas@users.noreply.github.com>
2025-08-07 12:03:17 +0100

    Add files via upload
    Update to include autostereogram generation.

--- photos3d/depthmaps.py
+++ photos3d/depthmaps.py
@@ -1,14 +1,18 @@
 """
-depthmaps.py - Library module for working with depth maps, such as for using them to convert RGB-D images to stereo pairs.
+depthmaps.py - Library module for working with depth maps, such as for using them to convert RGB-D images to stereo pairs or a stereogram.
 
 """
 
 import math
+import random
 import numpy as np
-from PIL import Image, ImageFilter
+from PIL import Image, ImageDraw, ImageFilter
 
+# GENERAL AND FILE FUNCTIONS
 
-def load(fn1, fn2=None, maxwid=None):
+def load(fn1, fn2=None, maxwid=None, dosplit=True):
+    if not dosplit:
+        return __open_image_file__(fn1, maxwid=maxwid)
     if fn2:
         img1 = __open_image_file__(fn1, maxwid=maxwid)
         img2 = __open_image_file__(fn2, maxwid=maxwid)
@@ -19,6 +23,19 @@ def load(fn1, fn2=None, maxwid=None):
             img1 = __open_image_file__(fn1, 2 * maxwid)
         img1, img2 = __split_image__(img1)
     return [img1, img2]
+
+def resize(img, nw, nh):
+    nim = img.resize((nw, nh))
+    return nim
+
+def split_image(img):
+    sbsw, sbsh = img.size
+    nw = int(sbsw/2)
+    left = img.crop((0, 0, nw, sbsh))
+    right = img.crop((sbsw-nw, 0, sbsw, sbsh))
+    return [left, right]
+
+# DISPARITY FUNCTIONS
 
 def estimate_disparity(img, strength='medium', converge='far'):
     mindisp = maxdisp = 0
@@ -47,32 +64,16 @@ def estimate_disparity(img, strength='medium', converge='far'):
             maxdisp = disp * (1 - converge)
     return [maxdisp, mindisp]
 
-def depth_image_to_array(dimg, ch='red', invert=False):
-    dw, dh = dimg.size
-    depth = np.zeros((dh, dw), dtype="float32") # (rows, cols)
-    dpix = dimg.load()
-    for y in range(0, dh):
-        for x in range(0, dw):
-            col = dpix[x, y]
-            if ch == 'red':
-                depth[y, x] = col[0]
-            elif ch == 'green':
-                depth[y, x] = col[1]
-            elif ch == 'blue':
-                depth[y, x] = col[2]
-            else:
-                depth[y, x] = (col[0] + col[1] + col[2]) / 3
-            if invert:
-                depth[y, x] = 255 - depth[y, x]
-    return depth
-
-def create_linear_disparity_vector(neardisp, fardisp):
+def create_linear_disparity_vector(neardisp, fardisp, doreverse=False):
     dispvec = []
     for c in range(0, 256):
-        dispvec.append(neardisp - ((c / 255) * (neardisp - fardisp)))
+        disp = neardisp - ((c / 255) * (neardisp - fardisp))
+        dispvec.append(disp)
+    if doreverse:
+        dispvec.reverse()
     return dispvec
 
-def create_tangent_disparity_vector(neardisp, cvg = 0.15, fac=1):
+def create_tangent_disparity_vector(neardisp, cvg = 0.15, fac=1, doreverse=False):
     # neardisp is in pixels.
     # conv is 0.05 to 1 * total depth.
     # dfac is 0.1 to 10 * total depth:
@@ -95,7 +96,11 @@ def create_tangent_disparity_vector(neardisp, cvg = 0.15, fac=1):
     df = neardisp / disps[0]
     for c in range(0, len(disps)):
         disps[c] = disps[c] * df
+    if doreverse:
+        disps.reverse()
     return disps
+
+# DEPTH BUFFER TO IMAGE FUNCTIONS
 
 def depth_to_stereo(rgbimg, depths, dispvect, prefill=True, blurfill=True, do3ddepth=True, dscl=None):
     """
@@ -117,9 +122,6 @@ def depth_to_stereo(rgbimg, depths, dispvect, prefill=True, blurfill=True, do3dd
         for x in range(0, imw):
             depth = depths[y, x]
             if do3ddepth:
-                ##########
-                # IS THIS THE RIGHT WAY TO COMPENSATE FOR DIFFERENT Z AXIS
-                # SCALE COMPARED TO IMAGE WIDTH (X) AND HEIGHT (Y) ??????
                 if dscl:
                     dscale = dscl
                 else:
@@ -151,6 +153,132 @@ def depth_to_stereo(rgbimg, depths, dispvect, prefill=True, blurfill=True, do3dd
                 npix[x + imw, y] = opix[rdx, y]
     return newimg
 
+def depth_to_autostereo(astile, numx, numy, depths, dispvect=None, helpers=True):
+    tile = image_copy(astile)
+    tw, th = tile.size
+    if dispvect == None:
+        dispvect = create_linear_disparity_vector(int(tw/2), 0, doreverse=True)
+        pass
+    edge = tile_image(tile, 1, numy)
+    imgin = tile_image(tile, numx, numy)
+    wid = numx * tw
+    hgt = numy * th
+    imgme = __blank_image__(wid, hgt, col=(0, 0, 0))
+    inpix = imgin.load()
+    mepix = imgme.load()
+    edpix = edge.load()
+    col = inpix[0, 0]
+    for y in range(0, hgt):
+        for x in range(0, wid):
+            dep = int(depths[y, x])
+            lrdisp = int(dispvect[dep])
+            dx = x + lrdisp - tw
+            if dx < 0:
+                col = edpix[tw + dx, y]
+            elif dx < wid:
+                col = mepix[dx, y]
+            mepix[x, y] = col
+    if helpers:
+        add_helpers(imgme, 50, dispvect[-1], 10, col=(0, 0, 0))
+    return imgme
+
+# DEPTH BUFFER FUNCTIONS
+
+def depth_image_to_array(dimg, ch='red', invert=False):
+    dw, dh = dimg.size
+    depth = np.zeros((dh, dw), dtype="float32") # (rows, cols)
+    dpix = dimg.load()
+    for y in range(0, dh):
+        for x in range(0, dw):
+            col = dpix[x, y]
+            if ch == 'red':
+                depth[y, x] = col[0]
+            elif ch == 'green':
+                depth[y, x] = col[1]
+            elif ch == 'blue':
+                depth[y, x] = col[2]
+            else:
+                depth[y, x] = (col[0] + col[1] + col[2]) / 3
+            if invert:
+                depth[y, x] = 255 - depth[y, x]
+    return depth
+
+def depth_buffer_remap(dbuf, tos, toe, frs=0, fre=255):
+    '''
+    ros and toe are range to map to.
+    frs and fre are range to map from.
+    '''
+    hgt, wid = dbuf.shape
+    for y in range(0, hgt):
+        for x in range(0, wid):
+            dep = dbuf[y, x]
+            dep = __map__(dep, frs, fre, tos, toe)
+            dep = __constrain__(dep, tos, toe)
+            dbuf[y, x] = dep
+
+def depth_buffer_clear_back(dbuf, cutoff, tod=0):
+    hgt, wid = dbuf.shape
+    for y in range(0, hgt):
+        for x in range(0, wid):
+            if dbuf[y, x] <= cutoff:
+                dbuf[y, x] = tod
+
+def depth_buffer_clear_front(dbuf, cutoff, tod=0):
+    hgt, wid = dbuf.shape
+    for y in range(0, hgt):
+        for x in range(0, wid):
+            if dbuf[y, x] >= cutoff:
+                dbuf[y, x] = tod
+
+def depth_buffer_clear_between(dbuf, dfrom, dto, tod=0):
+    hgt, wid = dbuf.shape
+    for y in range(0, hgt):
+        for x in range(0, wid):
+            if dbuf[y, x] >= dfrom:
+                if dbuf[y, x] <= dto:
+                    dbuf[y, x] = tod
+
+def depth_buffer_to_image(dbuf):
+    hgt, wid = dbuf.shape
+    dbimg = Image.new("RGB", (wid, hgt), (0, 0, 0))
+    dbpix = dbimg.load()
+    for y in range(0, hgt):
+        for x in range(0, wid):
+            col = dbuf[y, x]
+            dbpix[x, y] = (col, col, col)
+    return dbimg
+
+# STEREOGRAM FUNCTIONS
+
+def tile_image(img, nx, ny):
+    iw, ih = img.size
+    timg = Image.new("RGB", (iw*nx, ih*ny), (0, 0, 0))
+    for y in range(0, ny):
+        for x in range(0, nx):
+            timg.paste(img, (x*iw, y*ih))
+    return timg
+
+def make_random_pattern(pw, ph, rgb=True, odds=1, back=(255, 255, 255)):
+    pimg = Image.new("RGB", (pw, ph), back)
+    ppix = pimg.load()
+    for y in range(0, ph):
+        for x in range(0, pw):
+            if random.random() <= odds:
+                if rgb:
+                    ppix[x, y] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                else:
+                    col = random.randint(0, 255)
+                    ppix[x, y] = (col, col, col)
+    return pimg
+
+def add_helpers(img, top, sep, rad, col=(0, 0, 0)):
+    iw, ih = img.size
+    draw = ImageDraw.Draw(img)
+    x = (iw/ 2) - (sep / 2)
+    draw.ellipse(([x-rad, top-rad, x+rad, top+rad]), fill=col)
+    x = (iw/ 2) + (sep / 2)
+    draw.ellipse(([x-rad, top-rad, x+rad, top+rad]), fill=col)
+
 # PRIVATE HELPER FUNCTIONS
 
 def __map__(num, smin, smax, emin, emax):
@@ -177,7 +305,7 @@ def __open_image_file__(fname, maxwid=None):
         if w > mwid:
             asp = w / h
             nh = int(mwid / asp)
-            img = img.resize((mwid, nh), Image.LANCZOS)
+            img = img.resize((mwid, nh))
     if img.mode == "RGBA":
         img = img.convert('RGB')
     return img
@@ -195,6 +323,16 @@ def __blank_buffer__(rows, cols, dep=99999999):
         for x in range(0, cols):
             buff[y, x] = dep
     return buff
+
+def __blank_image__(wid, hgt, col=(0, 0, 0)):
+    img = Image.new("RGB", (wid, hgt), col)
+    return img
+
+def image_copy(img):
+    iw, ih = img.size
+    cimg = Image.new("RGB", (iw, ih), (0, 0, 0))
+    cimg.paste(img, (0, 0))
+    return cimg
 
 def __array_info__(arr):
     avg = 0
